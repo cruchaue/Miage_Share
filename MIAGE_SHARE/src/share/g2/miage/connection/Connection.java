878a5b27cd75f436c6fa550c63bf4454118cf6cb
Administrator <Administrator@130.190.29.168>
2014-04-15 09:54:23 +0200

    la partie de Connection

---
+++ MIAGE_SHARE/src/share/g2/miage/connection/Connection.java
@@ -0,0 +1,45 @@
+package share.g2.miage.connection;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public abstract class Connection {
+	protected ServerSocket serverSocket = null;
+	protected Socket socket = null;
+	protected OutputStream os = null;
+	protected InputStream is = null;
+
+	public Connection() {
+		// 监听端口号
+		int port = 10000;
+		try {
+			// 建立连接
+			serverSocket = new ServerSocket(port);
+			// 获得连接
+			socket = serverSocket.accept();
+			// 接收客户端发送内容
+			is = socket.getInputStream();
+			byte[] b = new byte[1024];
+			int n = is.read(b);
+			// 输出
+			System.out.println("客户端发送内容为：" + new String(b, 0, n));
+			// 向客户端发送反馈内容
+			os = socket.getOutputStream();
+			os.write(b, 0, n);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 关闭流和连接
+				os.close();
+				is.close();
+				socket.close();
+				serverSocket.close();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+}
---
+++ MIAGE_SHARE/src/share/g2/miage/connection/TCPMethod.java
@@ -0,0 +1,17 @@
+package share.g2.miage.connection;
+
+public class TCPMethod extends Connection implements FonctionShare{
+
+	@Override
+	public int uploadFichier(String fichierNom, String adresse) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int telechargerFichier(String fichierNom, String adresse) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
