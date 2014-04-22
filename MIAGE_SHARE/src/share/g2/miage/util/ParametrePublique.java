50dc5110ff7b01bbb179855b719eec38d1cf8422
Administrator <Administrator@PC--20140319QTE>
2014-04-22 16:28:55 +0200

    test

---
+++ MIAGE_SHARE/src/share/g2/miage/util/ParametrePublique.java
@@ -0,0 +1,34 @@
+package share.g2.miage.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ParametrePublique {
+	public final static String UPLOAD_FICHIER = "<@uploadFichier>";
+	public final static String TELECHARGER_FICHIER = "<@telechargerFichier>";
+	public final static String SUPPRIMER_FICHIER = "<@supprimerFichier>";
+	public final static String COMMENTER_FICHIER = "<@commenterFichier>";
+	public final static String CREER_UTILISATEUR = "<@creerUtilisateur>";
+	public final static String LOGIN = "<@login>";
+	public final static String LIRE_FICHIER_INFO = "<@lireFichierInfo>";
+	public final static String USER_EXISTE_PAS = "<@UserExistePas>";
+	public final static String USER_PW_PAS_CORRECTE = "<@pwPasCorrecte>";
+	public final static String OK = "<@ok>";
+	
+	public final static String ENVOYER_MESSAGE = "<@envoyerMessage>";
+	public final static String RECEVOIR_MESSAGE = "<@recevoirMessage>";
+	public final static String DISTRIBUER_MESSAGE = "<@distribuerMessage>";
+	
+	public final static String SPEPARER_FICHIER_INFO = "<@_SFI>";
+
+	public final static String SPEPARER_FICHIER_COMMENTAIRE1 = "<@_SC>";
+	public final static String SPEPARER_FICHIER_COMMENTAIRE2 = "<@_SC_>";
+	public final static String SPEPARER_INFO_UTILISATEUR = "<@_SIU>";
+
+	
+	public final static String UPLOAD_FINIT = "<@finit>";
+	public final static int LENGTH_ENVOYER = 1024;
+
+	
+}
