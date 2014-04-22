82741e9bb6618ab16d698cff9835920de6e72678
cruchaue <Emerick-OM@hotmail.fr>
2014-04-22 11:24:28 +0200

    Chat3

---
+++ MIAGE_SHARE/src/share/g2/miage/connectionClient/dao/Chat.java
@@ -0,0 +1,18 @@
+package share.g2.miage.connectionClient.dao;
+
+public class Chat {
+	private String user;
+	private String contenu;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}	
+	public String getContenu() {
+		return contenu;
+	}
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+}
---
+++ MIAGE_SHARE/src/share/g2/miage/connectionClient/fonction/EnvoyerMessageChat.java
@@ -0,0 +1,16 @@
+package share.g2.miage.connectionClient.fonction;
+
+import java.io.DataOutputStream;
+
+import share.g2.miage.connectionClient.dao.Client;
+import share.g2.miage.util.ParametrePublique;
+
+public class EnvoyerMessageChat {
+
+	public int excuter(Client client) {
+		
+
+
+		return 1;
+	}
+}
