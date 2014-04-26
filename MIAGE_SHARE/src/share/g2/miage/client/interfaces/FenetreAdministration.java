d2c956760c56f72bc683ec5efaeae38640ecac40
Olivier <Olivier@Olivier-PC>
2014-04-26 15:02:54 +0200

    supprimier utilisateur v1

--- MIAGE_SHARE/src/share/g2/miage/client/interfaces/FenetreAdministration.java
+++ MIAGE_SHARE/src/share/g2/miage/client/interfaces/FenetreAdministration.java
@@ -4,34 +4,40 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Properties;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.DefaultListModel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JTabbedPane;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JList;
+import javax.swing.JButton;
 
+import share.g2.miage.util.Parametre;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class FenetreAdministration extends JFrame{
+public class FenetreAdministration extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel container = new JPanel();
 	private JPanel pan1 = new JPanel();
 	private JPanel pan2 = new JPanel();
 	private JPanel pan3 = new JPanel();
 	private JTabbedPane onglet = new JTabbedPane();
 	private final DefaultListModel<String> model;
-
+	private BufferedReader br;
 
 	/**
 	 * Create the frame.
@@ -39,11 +45,11 @@ public class FenetreAdministration extends JFrame{
 	public FenetreAdministration() {
 		this.setTitle("TESTE");
 		this.setSize(700, 600);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		container.setBackground(Color.white);
 		container.setLayout(new BorderLayout());
-		//*************les onglets*****************************************//
+		// *************les onglets*****************************************//
 		onglet.addTab("onglet1", null, pan1);
 		pan1.setLayout(null);
 
@@ -51,46 +57,74 @@ public class FenetreAdministration extends JFrame{
 		scrollPane.setBounds(74, 37, 504, 323);
 		pan1.add(scrollPane);
 
-		final JList list = new JList(new DefaultListModel<String>());;
+		final JList<String> list = new JList<String>(
+				new DefaultListModel<String>());
+		;
 		scrollPane.setViewportView(list);
 
 		model = new DefaultListModel<String>();
 		scrollPane.setViewportView(list);
 		list.setModel(model);
+		final Properties p = new Properties();
+		System.out.println(p.getProperty("BD_utilisateurs"));
 
-		onglet.addTab("onglet2", null,pan2);
-		onglet.addTab("onglet3", null,pan3);
-		onglet.setTitleAt(0,"Gestion utilisateurs");
-		onglet.setTitleAt(1,"Gestion fichiers ");
-		onglet.setTitleAt(2,"Gestion des groupes ");
+		JButton btnNewButton = new JButton("Modifier");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+				
+			}
+		});
+		btnNewButton.setBounds(74, 382, 115, 41);
+		pan1.add(btnNewButton);
+
+		JButton btnSupprimer = new JButton("Supprimer");
+		btnSupprimer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				deleteLine(Parametre.fichiers_BD_utilisateurs,list.getSelectedIndex());
+			}
+		});
+		btnSupprimer.setBounds(223, 382, 115, 41);
+		pan1.add(btnSupprimer);
+
+		onglet.addTab("onglet2", null, pan2);
+		onglet.addTab("onglet3", null, pan3);
+		onglet.setTitleAt(0, "Gestion utilisateurs");
+		onglet.setTitleAt(1, "Gestion fichiers ");
+		onglet.setTitleAt(2, "Gestion des groupes ");
 
 		listerUtilisateurs();
 
-		//***********et tu peux ajouter d'autre************************//
-		container.add(onglet,null);
+		// ***********et tu peux ajouter d'autre************************//
+		container.add(onglet, null);
 
-		this.setContentPane(container);          
+		this.setContentPane(container);
 
 		this.setVisible(true);
 	}
 
-
 	public void listerUtilisateurs() {
 		String fichiers_BD_utilisateurs = ClientInterface.getBD_utilisateurs();
 
-		File filename = new File(fichiers_BD_utilisateurs); 
+		File filename = new File(fichiers_BD_utilisateurs);
 		InputStreamReader reader;
 		try {
 			reader = new InputStreamReader(new FileInputStream(filename));
 
-			BufferedReader br = new BufferedReader(reader); 
+			br = new BufferedReader(reader);
 			String line = "";
+			
 
-			while ((line =br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				System.out.println(line);
 				String uStr[] = line.split(";");
-				model.addElement(uStr[1]);
+				model.addElement(uStr[0]);
+				
+	
+
 			}
+			
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -101,10 +135,31 @@ public class FenetreAdministration extends JFrame{
 		}
 	}
 
+	public void deleteLine(String fileName, int lineNumber) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					new FileInputStream(fileName)));
 
+			StringBuffer sb = new StringBuffer();
+			String line;
+			int nbLinesRead = 0;
+			while ((line = reader.readLine()) != null) {
+				if (nbLinesRead != lineNumber) {
+					sb.append(line + "\r\n");
+				}
+				nbLinesRead++;
+			}
+			reader.close();
+			BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+			out.write(sb.toString());
+			out.close();
+			System.out.println("done");
 
+		} catch (Exception e) {
 
+		}
 
+	}
 
 	/**
 	 * Launch the application.
@@ -122,6 +177,3 @@ public class FenetreAdministration extends JFrame{
 		});
 	}
 }
-
-
-
