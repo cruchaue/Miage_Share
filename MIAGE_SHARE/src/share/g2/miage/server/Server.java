333d53f08b2c32f1c7f83bcd537e82d34a770273
Olivier <Olivier@130.190.31.52>
2014-05-05 14:15:52 +0200

    ests

---
+++ MIAGE_SHARE/src/share/g2/miage/server/Server.java
@@ -0,0 +1,25 @@
+package share.g2.miage.server;
+
+import share.g2.miage.server.ServerChat;
+import share.g2.miage.server.ServerFichier;
+
+/**
+ * Server est la classe contenant la m�thode main permettant de mettre en route 
+ * les serveurs de gestions des fichiers ainsi que celui du chats.
+ *
+ */
+public class Server {
+
+	/**
+	 * Constructeur qui permet de mettre en route les serveurs de fichiers et de chats.
+	 * @param args
+	 * 
+	 * @throws Exception Si un probl�me est apparu lors du lancement du serveur
+	 */
+	public Server(){
+		new ServerFichier();
+		new ServerChat();
+		
+	}
+
+}
