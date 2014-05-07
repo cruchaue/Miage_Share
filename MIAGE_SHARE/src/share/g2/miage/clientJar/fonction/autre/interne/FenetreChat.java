14e75bd2e9a4f6f54e27cdde8bafb29f3e4f0f90
olmai <olmai@PCNT214-02.e-ima.ujf-grenoble.fr>
2014-05-07 11:23:25 +0200

    sfdsf

--- MIAGE_SHARE/src/share/g2/miage/clientJar/fonction/autre/interne/FenetreChat.java
+++ MIAGE_SHARE/src/share/g2/miage/clientJar/fonction/autre/interne/FenetreChat.java
@@ -1,42 +1,51 @@
 package share.g2.miage.clientJar.fonction.autre.interne;
-import java.awt.BorderLayout;  
-import java.awt.Button;  
-import java.awt.TextArea;  
-import java.awt.TextField;  
-import java.awt.event.ActionEvent;  
-import java.awt.event.ActionListener;  
-import java.io.BufferedReader;  
-import java.io.IOException;  
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;  
-import java.io.PrintStream;  
-import java.net.Socket;  
-import java.net.UnknownHostException;  
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Properties;
 
-import javax.swing.JFrame;  
+import javax.swing.JFrame;
 
 import share.g2.miage.client.outil.ParametreC;
 
-@SuppressWarnings("serial")  
-public class FenetreChat extends JFrame  implements Runnable {  
-	private TextArea textArea = new TextArea();  
-	private TextField textField = new TextField(); 
-	
-	private String userName;
-	private Button button_send = new Button("Envoyer");  
-	private Button button_clear = new Button("Effacer");  
+@SuppressWarnings("serial")
+public class FenetreChat extends JFrame implements Runnable {
+	private TextArea textArea = new TextArea();
+	private TextField textField = new TextField();
 
-	private static Socket clientLink;  
-	private BufferedReader br;  
-	private PrintStream ps;  
-	private String msg;  
-	private StringBuffer sb = new StringBuffer();  
-	
-	static {  
-		try {  
-			
-			InputStream inputStream = FenetreChat.class.getClass().getClassLoader()
+	private String userName;
+	private Button button_send = new Button("Envoyer");
+	private Button button_clear = new Button("Effacer");
+
+	private static Socket clientLink;
+	private BufferedReader br;
+	private PrintStream ps;
+	private String msg;
+	private StringBuffer sb = new StringBuffer();
+
+	public FenetreChat(String userName) {
+		super("ChatRoom");
+		this.userName = userName;
+	}
+
+	@Override
+	public void run() {
+
+		try {
+
+			InputStream inputStream = this.getClass()
+					.getClassLoader()
 					.getResourceAsStream("ipConfig.properties");
 			Properties p = new Properties();
 			try {
@@ -45,102 +54,91 @@ public class FenetreChat extends JFrame  implements Runnable {
 				e1.printStackTrace();
 			}
 			String serverIp = p.getProperty("serverIp");
-			
-			clientLink = new Socket(serverIp,2001);  
-		} catch (UnknownHostException e) {  
-			e.printStackTrace();  
-		} catch (IOException e) {  
-			e.printStackTrace();  
-		}  
-	}  
 
-	public FenetreChat(String userName) {  
-		super("ChatRoom");  
-		this.userName = userName;
-	}  
+			clientLink = new Socket(serverIp, 2001);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+		this.setLocation(400, 450);
+		this.setSize(400, 400);
+		this.add(textArea, BorderLayout.NORTH);//
+		this.add(textField, BorderLayout.SOUTH);//
+		this.add(button_send, BorderLayout.EAST);//
+		this.add(button_clear, BorderLayout.WEST);
+		this.setVisible(true);//
+		this.pack();//
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		try {
 
+			br = new BufferedReader(new InputStreamReader(
+					clientLink.getInputStream()));
+			ps = new PrintStream(clientLink.getOutputStream());
 
-	@Override
-	public void run() {
-		this.setLocation(400, 450);  
-		this.setSize(400, 400);  
-		this.add(textArea, BorderLayout.NORTH);//   
-		this.add(textField, BorderLayout.SOUTH);//  
-		this.add(button_send, BorderLayout.EAST);//   
-		this.add(button_clear, BorderLayout.WEST);  
-		this.setVisible(true);//   
-		this.pack();//  
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-
-		try {  
-
-			br = new BufferedReader(new InputStreamReader(clientLink  
-					.getInputStream()));  
-			ps = new PrintStream(clientLink.getOutputStream());  
-
-			//   
-			button_send.addActionListener(new ActionListener() {  
-				@Override  
+			//
+			button_send.addActionListener(new ActionListener() {
+				@Override
 				public void actionPerformed(ActionEvent e) {
-					msg = textField.getText();  
+					msg = textField.getText();
 
-					ps.println(userName+": "+msg);  
-					ps.flush();  
-				}  
+					ps.println(userName + ": " + msg);
+					ps.flush();
+				}
 
-			});  
+			});
 
-			textField.addActionListener(new ActionListener() {  
-				@Override  
-				public void actionPerformed(ActionEvent e) {  
-					msg = textField.getText();  
+			textField.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					msg = textField.getText();
 
-					ps.println(userName+": "+msg);  
-					ps.flush();  
+					ps.println(userName + ": " + msg);
+					ps.flush();
 
-				}  
+				}
 
-			});  
+			});
 
-			button_clear.addActionListener(new ActionListener() {  
-				@Override  
-				public void actionPerformed(ActionEvent e) {  
-					textArea.setText("");  
-					sb.delete(0, sb.length());  
-				}  
-			});  
+			button_clear.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					textArea.setText("");
+					sb.delete(0, sb.length());
+				}
+			});
 
-			while (true) {  
-				msg = br.readLine();  
-				sb.append(msg + "\n");  
-				textArea.setText(sb.toString());  
-				textField.setText("");  
-			}  
+			while (true) {
+				msg = br.readLine();
+				sb.append(msg + "\n");
+				textArea.setText(sb.toString());
+				textField.setText("");
+			}
 
-		} catch (UnknownHostException e) {  
-			e.printStackTrace();  
-		} catch (IOException e) {  
-			e.printStackTrace();  
-		} finally {  
-			try {  
-				if (null != clientLink) {  
-					clientLink.close();  
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != clientLink) {
+					clientLink.close();
 
-				}  
-				if (null != br) {  
-					br.close();  
-				}  
-				if (null != ps) {  
-					ps.close();  
-				}  
+				}
+				if (null != br) {
+					br.close();
+				}
+				if (null != ps) {
+					ps.close();
+				}
 
-			} catch (IOException e) {  
-				e.printStackTrace();  
-			}  
-		}  
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-	}  
+	}
 
-}  
+}
